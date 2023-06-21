727324b656bf357ed418b1e85874147a4557f4e1
Abhiorange <soniabhi0510@gmail.com>
2023-06-21 17:45:53 +0530

    feag

---
+++ RestaurantWebApi/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿/*using Microsoft.AspNetCore.Mvc;
+using ViewModels.Models;
+
+namespace RestaurantWebApi.Areas.admin.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class AdminController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(CompanyLoginVm model)
+        {
+            return Ok(); 
+        }
+       
+    }
+}
+*/
